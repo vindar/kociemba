2bb6c57c812c4ae4a1a05d23210a4f12b32c0812
Arvind Singh <vindar@users.noreply.github.com>
2021-08-28 00:11:58 +0200

    typo

--- examples/test_kociemba/test_kociemba.ino
+++ examples/test_kociemba/test_kociemba.ino
@@ -24,41 +24,41 @@
 
 // 5 randomly scrambled cube. 
 char test_cube[5][55] = {
-	"UBRDUFUDLBUBFRDFRDFLURFRBFDDLRLDDFBLRBLULBLURUUFFBRBLD",
-	"RDRBUFFUUFRFDRULRFDRLBFBBFBDUDFDDBBRURLDLULLRDLBLBFULU",
-	"DRLUUBFBRBLURRLRUBLRDDFDLFUFUFFDBRDUBRUFLLFDDBFLUBLRBD",
-	"DLFRUUURLUBDLRBLURBBFLFDFFUURBFDRBBDLDRDLFDURRUFDBFBLL",
-	"BUFUUDFBLURRFRLRFBDRFUFRBBFRDDFDDBULLLLDLBRLUURDBBLUFD"
-	};
+    "UBRDUFUDLBUBFRDFRDFLURFRBFDDLRLDDFBLRBLULBLURUUFFBRBLD",
+    "RDRBUFFUUFRFDRULRFDRLBFBBFBDUDFDDBBRURLDLULLRDLBLBFULU",
+    "DRLUUBFBRBLURRLRUBLRDDFDLFUFUFFDBRDUBRUFLLFDDBFLUBLRBD",
+    "DLFRUUURLUBDLRBLURBBFLFDFFUURBFDRBBDLDRDLFDURRUFDBFBLL",
+    "BUFUUDFBLURRFRLRFBDRFUFRBBFRDDFDDBULLLLDLBRLUURDBBLUFD"
+    };
 
 
 elapsedMillis em;
 
-DMAMEM char buf479[479*1024];	// 479K in DMAMEM
-char  buf248[248 * 1024];	// 248K on the stack 
+DMAMEM char buf479[479*1024];   // 479K in DMAMEM
+char  buf248[248 * 1024];       // 248K on in DTCM 
 
 
 void setup() 
-	{
-	while (!Serial);
+    {
+    while (!Serial);
   em=0;
-	kociemba::set_memory(buf479, buf248);	// removing this line slows the computation by a factor of 4 (but saves a lot of RAM...)
-	Serial.printf("RAM buffer created in %d ms.\n", (int)em);
-	}
+    kociemba::set_memory(buf479, buf248);   // removing this line slows the computation by a factor of 4 (but saves a lot of RAM...)
+    Serial.printf("RAM buffer created in %d ms.\n", (int)em);
+    }
 
 
 void loop() 
-	{
-	for (auto s : test_cube)
-		{
-		em = 0;
-		const char* res = kociemba::solve(s);
-		if (res == nullptr)
-			Serial.println("no solution found :(");
-		else
-			Serial.printf("Solution [%s] found in %d ms.\n", res, (int)em);
-		}
-	}
+    {
+    for (auto s : test_cube)
+        {
+        em = 0;
+        const char* res = kociemba::solve(s);
+        if (res == nullptr)
+            Serial.println("no solution found :(");
+        else
+            Serial.printf("Solution [%s] found in %d ms.\n", res, (int)em);
+        }
+    }
 
 /* end of file */
 
